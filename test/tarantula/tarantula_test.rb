b99cc8e4c02a0ad034d0ac51bfe3c854b4d6e62f
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-10-20 15:00:20 +0200

    uncomment tarantula tests for other roles

--- test/tarantula/tarantula_test.rb
+++ test/tarantula/tarantula_test.rb
@@ -23,11 +23,11 @@ class TarantulaTest < ActionDispatch::IntegrationTest
     crawl_as(people(:top_leader))
   end
 
-  def xtest_tarantula_as_regionalleitung
+  def test_tarantula_as_regionalleitung
     crawl_as(people(:regio_leader))
   end
 
-  def xtest_tarantula_as_aktivmitglied
+  def test_tarantula_as_aktivmitglied
     crawl_as(people(:regio_aktiv))
   end
 
