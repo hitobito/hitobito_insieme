0415df83dc89364741bd7a6ab7aca93f219529d0
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-10-20 16:16:27 +0200

    add missing license headers

--- app/models/event/course/role/expert.rb
+++ app/models/event/course/role/expert.rb
@@ -1,4 +1,11 @@
 # encoding: utf-8
+
+#  Copyright (c) 2012-2014, insieme Schweiz. This file is part of
+#  hitobito_insieme and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_insieme.
+
+
 # == Schema Information
 #
 # Table name: event_roles
@@ -8,12 +15,6 @@
 #  participation_id :integer          not null
 #  label            :string(255)
 #
-
-
-#  Copyright (c) 2012-2014, insieme Schweiz. This file is part of
-#  hitobito_insieme and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_insieme.
 
 class Event::Course::Role::Expert <  Event::Role
 
--- spec/models/event/course_record_spec.rb
+++ spec/models/event/course_record_spec.rb
@@ -1,4 +1,10 @@
 # encoding: utf-8
+
+#  Copyright (c) 2012-2014, insieme Schweiz. This file is part of
+#  hitobito_insieme and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_insieme.
+
 # == Schema Information
 #
 # Table name: event_course_records
@@ -32,11 +38,6 @@
 #  gemeinkosten_updated_at          :datetime
 #  zugeteilte_kategorie             :string(2)
 #
-
-#  Copyright (c) 2012-2014, insieme Schweiz. This file is part of
-#  hitobito_insieme and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_insieme.
 
 require 'spec_helper'
 
