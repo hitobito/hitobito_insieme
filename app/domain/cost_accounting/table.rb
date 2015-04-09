99ca98bb5627a7ef4ccc222c61160eafc5996f3b
Mathis Hofer <hofer@puzzle.ch>
2015-04-09 10:20:19 +0200

    kr: add deckungsbeitrag2, closes #7569

--- app/domain/cost_accounting/table.rb
+++ app/domain/cost_accounting/table.rb
@@ -30,7 +30,7 @@ module CostAccounting
                 CostAccounting::Report::DirekteSpendenAusserhalb,
                 CostAccounting::Report::TotalErtraege,
                 CostAccounting::Report::Deckungsbeitrag1,
-                # CostAccounting::Report::Deckungsbeitrag2,
+                CostAccounting::Report::Deckungsbeitrag2,
                 # CostAccounting::Report::Deckungsbeitrag3,
                 # CostAccounting::Report::Deckungsbeitrag4,
                ].each_with_object({}) { |r, hash| hash[r.key] = r }
