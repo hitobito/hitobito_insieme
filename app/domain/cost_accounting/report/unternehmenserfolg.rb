1a9940a191eb8db65b760d78abd6b1a80e6a135c
Mathis Hofer <hofer@puzzle.ch>
2015-04-09 11:45:55 +0200

    kr: add unternehmenserfolg, closes #7572

---
+++ app/domain/cost_accounting/report/unternehmenserfolg.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2014, insieme Schweiz. This file is part of
+#  hitobito_insieme and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_insieme.
+
+module CostAccounting
+  module Report
+    class Unternehmenserfolg < Base
+      self.kind = :unternehmenserfolg
+
+      def aufwand_ertrag_ko_re
+        nil
+      end
+
+      def total
+        @total ||= table.value_of('total_ertraege', 'aufwand_ertrag_fibu').to_d - \
+                   table.value_of('total_aufwand', 'aufwand_ertrag_fibu').to_d
+      end
+
+      def kontrolle
+        nil
+      end
+    end
+  end
+end
