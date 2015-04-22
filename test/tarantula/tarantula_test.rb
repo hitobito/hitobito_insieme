8f72ac0c55b5b3444370cfa07442c2ba0eda778d
Andreas Maierhofer <maierhofer@puzzle.ch>
2015-04-22 13:18:42 +0200

    fix tarantula

--- test/tarantula/tarantula_test.rb
+++ test/tarantula/tarantula_test.rb
@@ -38,6 +38,7 @@ class TarantulaTest < ActionDispatch::IntegrationTest
 
     t.skip_uri_patterns << /groups\/\d+\/cost_accounting\/#{outside_three_years_window}/
     t.skip_uri_patterns << /groups\/\d+\/time_record\/#{outside_three_years_window}/
+    t.skip_uri_patterns << /groups\/\d+\/capital_substrate\/#{outside_three_years_window}/
 
     # already deleted in another language
     t.allow_404_for << /reporting_parameters\/\d+$/
