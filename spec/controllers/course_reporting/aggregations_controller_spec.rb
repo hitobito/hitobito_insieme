f7b57740bd982fed7ce6dcb4f59319eddba1960e
Matthias Viehweger <viehweger@puzzle.ch>
2020-04-16 12:55:13 +0200

    Adapt spec to cover current Content-Disposition-Header

--- spec/controllers/course_reporting/aggregations_controller_spec.rb
+++ spec/controllers/course_reporting/aggregations_controller_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-#  Copyright (c) 2015, insieme Schweiz. This file is part of
+#  Copyright (c) 2015-2020, insieme Schweiz. This file is part of
 #  hitobito_insieme and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_insieme.
@@ -23,13 +23,25 @@ describe CourseReporting::AggregationsController do
   it 'export renders ok' do
     get :export, params: { id: group.id, year: 2014, lk: 'bk', subsidized: 'true', categories: [1,2,3] }
     expect(response).to be_ok
-    expect(response.headers["Content-Disposition"]).to eq "attachment; filename=\"course_statistics_insieme-schweiz_2014_blockkurse_subsidized_1_2_3.csv\""
+
+    filename = 'course_statistics_insieme-schweiz_2014_blockkurse_subsidized_1_2_3.csv'
+
+    header = response.headers["Content-Disposition"]
+    expect(header).to match(/^attachment;/)
+    expect(header).to match(/filename=\"#{filename}\";/)
+    expect(header).to match(/filename\*=UTF-8''#{filename}/)
   end
 
   it 'export renders consolidated if allowed' do
     get :export, params: { id: group.id, year: 2014, lk: 'bk', subsidized: 'true', categories: [1,2,3], consolidate: true }
     expect(response).to be_ok
-    expect(response.headers["Content-Disposition"]).to eq "attachment; filename=\"course_statistics_insieme-schweiz_2014_blockkurse_subsidized_1_2_3_consolidated.csv\""
+
+    filename = "course_statistics_insieme-schweiz_2014_blockkurse_subsidized_1_2_3_consolidated.csv"
+
+    header = response.headers["Content-Disposition"]
+    expect(header).to match(/^attachment;/)
+    expect(header).to match(/filename=\"#{filename}\";/)
+    expect(header).to match(/filename\*=UTF-8''#{filename}/)
   end
 
   it 'export does not render consolidated if not allowed' do
@@ -37,7 +49,13 @@ describe CourseReporting::AggregationsController do
 
     get :export, params: { id: groups(:be).id, year: 2014, lk: 'bk', subsidized: 'true', categories: [1,2,3], consolidate: true }
     expect(response).to be_ok
-    expect(response.headers["Content-Disposition"]).to eq "attachment; filename=\"course_statistics_kanton-bern_2014_blockkurse_subsidized_1_2_3.csv\""
+
+    filename = "course_statistics_kanton-bern_2014_blockkurse_subsidized_1_2_3.csv"
+
+    header = response.headers["Content-Disposition"]
+    expect(header).to match(/^attachment;/)
+    expect(header).to match(/filename=\"#{filename}\";/)
+    expect(header).to match(/filename\*=UTF-8''#{filename}/)
   end
 
 end
