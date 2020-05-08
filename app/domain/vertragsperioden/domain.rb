60f28d7b9b1f002981ef34e4acc7b096f360b966
Matthias Viehweger <viehweger@puzzle.ch>
2020-05-08 14:19:41 +0200

    Add Vertragsperioden-Dispatcher and helper-classes

---
+++ app/domain/vertragsperioden/domain.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2020, Insieme Schweiz. This file is part of hitobito_insieme
+#  and licensed under the Affero General Public License version 3 or later.
+#  See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_insieme.
+
+module Vertragsperioden
+  module Domain
+    def vertragsperiode
+      @vertragsperiode ||= begin
+                             raise Vertragsperioden::NoYearError unless year.present?
+                             Vertragsperioden::Dispatch.new(year)
+                           end
+    end
+
+    def vp_module(class_name)
+      vertragsperiode.domain_module(class_name)
+    end
+  end
+end
