813ee38856e392bea7a4b031e6260807ea5935ae
Carlo Beltrame <carlo@beltra.me>
2020-05-20 10:00:31 +0200

    Add specs for Vollkosten pro Betreuungsstunde in VP2020

---
+++ spec/vp2020/models/event/course_record_spec.rb
@@ -0,0 +1,71 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2014, insieme Schweiz. This file is part of
+#  hitobito_insieme and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_insieme.
+
+require 'spec_helper'
+
+describe Event::CourseRecord do
+
+  let(:group) { groups(:be) }
+
+  def new_record(event, attrs = {})
+    event.course_record.try(:destroy!)
+    r = Event::CourseRecord.new(attrs.merge(event: event))
+    r.valid?
+    r
+  end
+
+  context 'treffpunkt courses' do
+    let(:event) { Fabricate(:course, groups: [group], leistungskategorie: 'tp', fachkonzept: 'treffpunkt', year: 2020) }
+    let(:record) do
+      new_record(event)
+    end
+
+    before do
+      event.betreuerinnen = 2
+      event.kursdauer = 2
+      event.direkter_aufwand = 60
+      event.gemeinkostenanteil = 20
+    end
+
+    it 'calculates vollkosten pro betreuungsstunde' do
+      expect(record.vollkosten_pro_betreuungsstunde).to eq(20.to_d)
+    end
+
+    it 'handles division by zero' do
+      event.betreuerinnen = 0
+      expect(record.vollkosten_pro_betreuungsstunde).to eq(0.to_d)
+    end
+
+  end
+
+  context 'treffpunkt aggregate courses' do
+    let(:event) do
+      Fabricate(:aggregate_course, groups: [group], leistungskategorie: 'tp', fachkonzept: 'treffpunkt', year: 2020)
+    end
+    let(:record) do
+      new_record(event)
+    end
+
+    before do
+      event.betreuerinnen = 2
+      event.kursdauer = 2
+      event.direkter_aufwand = 60
+      event.gemeinkostenanteil = 20
+    end
+
+    it 'calculates vollkosten pro betreuungsstunde' do
+      expect(record.vollkosten_pro_betreuungsstunde).to eq(20.to_d)
+    end
+
+    it 'handles division by zero' do
+      event_tp.betreuerinnen = 0
+      expect(record.vollkosten_pro_betreuungsstunde).to eq(0.to_d)
+    end
+
+  end
+
+end
