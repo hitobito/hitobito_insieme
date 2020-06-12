c0b141ad342ab08ad588e87c399bde02f2da02a5
Matthias Viehweger <viehweger@puzzle.ch>
2020-06-12 06:36:28 +0200

    Add VP2020 for export of timerecords

--- spec/regressions/time_records_controller_spec.rb
+++ spec/regressions/time_records_controller_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-#  Copyright (c) 2016, insieme Schweiz. This file is part of
+#  Copyright (c) 2016-2020, insieme Schweiz. This file is part of
 #  hitobito_insieme and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_insieme.
@@ -30,6 +30,14 @@ describe TimeRecordsController, type: :controller  do
       csv = response.body
       expect(csv).to match(/\A;Zeiterfassung Angestellte;Zeiterfassung Ehrenamtliche mit Leistungsnachweis;Zeiterfassung Ehrenamtliche ohne Leistungsnachweis/)
       expect(csv).to match(/^Art\. 74 betreffend in 100% Stellen;;;$/)
+    end
+
+    it 'exports csv for 2020' do
+      get :index, params: { id: groups(:dachverein).id, year: 2020 }, format: :csv
+      csv = response.body
+      expect(csv).to match(/\A;Zeiterfassung Angestellte;Zeiterfassung Ehrenamtliche mit Leistungsnachweis;Zeiterfassung Ehrenamtliche ohne Leistungsnachweis/)
+      expect(csv).to match(/^Art\. 74 betreffend in 100% Stellen;;;$/)
+      expect(csv).to match(/^Grundlagenarbeit zu LUFEB/)
     end
   end
 
