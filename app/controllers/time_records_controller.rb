4b5ce607f61ce924f797347eabea80f756d9930c
Matthias Viehweger <viehweger@puzzle.ch>
2020-06-12 06:32:53 +0200

    Conform to linter and styleguide

--- app/controllers/time_records_controller.rb
+++ app/controllers/time_records_controller.rb
@@ -1,52 +1,5 @@
 # frozen_string_literal: true
 
-# == Schema Information
-#
-# Table name: time_records
-#
-#  id                                       :integer          not null, primary key
-#  group_id                                 :integer          not null
-#  year                                     :integer          not null
-#  verwaltung                               :integer
-#  beratung                                 :integer
-#  treffpunkte                              :integer
-#  blockkurse                               :integer
-#  tageskurse                               :integer
-#  jahreskurse                              :integer
-#  kontakte_medien                          :integer
-#  interviews                               :integer
-#  publikationen                            :integer
-#  referate                                 :integer
-#  medienkonferenzen                        :integer
-#  informationsveranstaltungen              :integer
-#  sensibilisierungskampagnen               :integer
-#  auskunftserteilung                       :integer
-#  kontakte_meinungsbildner                 :integer
-#  beratung_medien                          :integer
-#  eigene_zeitschriften                     :integer
-#  newsletter                               :integer
-#  informationsbroschueren                  :integer
-#  eigene_webseite                          :integer
-#  erarbeitung_instrumente                  :integer
-#  erarbeitung_grundlagen                   :integer
-#  projekte                                 :integer
-#  vernehmlassungen                         :integer
-#  gremien                                  :integer
-#  vermittlung_kontakte                     :integer
-#  unterstuetzung_selbsthilfeorganisationen :integer
-#  koordination_selbsthilfe                 :integer
-#  treffen_meinungsaustausch                :integer
-#  beratung_fachhilfeorganisationen         :integer
-#  unterstuetzung_behindertenhilfe          :integer
-#  mittelbeschaffung                        :integer
-#  allgemeine_auskunftserteilung            :integer
-#  type                                     :string(255)      not null
-#  total_lufeb_general                      :integer
-#  total_lufeb_private                      :integer
-#  total_lufeb_specific                     :integer
-#  total_lufeb_promoting                    :integer
-#  nicht_art_74_leistungen                  :integer
-
 #  Copyright (c) 2012-2020, insieme Schweiz. This file is part of
 #  hitobito_insieme and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
@@ -56,7 +9,7 @@ class TimeRecordsController < ReportingBaseController
 
   TYPES = [TimeRecord::EmployeeTime,
            TimeRecord::VolunteerWithVerificationTime,
-           TimeRecord::VolunteerWithoutVerificationTime]
+           TimeRecord::VolunteerWithoutVerificationTime].freeze
 
   include Rememberable
   include Vertragsperioden::Views
