f3e5147d23850192e4b2b7f1c22ba78fd066c33a
Matthias Viehweger <viehweger@puzzle.ch>
2020-06-17 15:15:19 +0200

    Reload Person-model to have current column-information

--- spec/support/reload_column_information.rb
+++ spec/support/reload_column_information.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2019, insieme Schweiz. This file is part of
+#  Copyright (c) 2019-2020, insieme Schweiz. This file is part of
 #  hitobito_sbv and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sbv.
 
 Event::CourseRecord.reset_column_information
+Person.reset_column_information
