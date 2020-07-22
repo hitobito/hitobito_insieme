6c1a0c1143da1b61654f1ac57fd188450904c26f
Matthias Viehweger <viehweger@puzzle.ch>
2020-07-22 18:40:47 +0200

    Allow to and override Tableheaders for VP2020

--- app/helpers/cost_accounting_helper.rb
+++ app/helpers/cost_accounting_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2014, insieme Schweiz. This file is part of
+#  Copyright (c) 2012-2020, insieme Schweiz. This file is part of
 #  hitobito_insieme and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_insieme.
@@ -31,6 +31,14 @@ module CostAccountingHelper
 
   def cost_accounting_reports
     vp_class('CostAccounting::Table')::VISIBLE_REPORTS
+  end
+
+  def cost_accounting_report_header(field)
+    I18n.translate(
+      field,
+      scope: [vp_i18n_scope, :headers],
+      default: CostAccountingRecord.human_attribute_name(field)
+    )
   end
 
   def base_time_record_group_path(group, _params = {})
