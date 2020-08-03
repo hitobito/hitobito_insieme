ec5f054042196ba0e0546a74c2b10e0230a31b5a
Matthias Viehweger <viehweger@puzzle.ch>
2020-08-03 15:57:15 +0200

    CacheStore#clear takes only options, not a key to clear

--- app/models/global_value.rb
+++ app/models/global_value.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2015, insieme Schweiz. This file is part of
+#  Copyright (c) 2012-2020, insieme Schweiz. This file is part of
 #  hitobito_insieme and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_insieme.
@@ -39,7 +39,7 @@ class GlobalValue < ActiveRecord::Base
     end
 
     def clear_cache
-      Rails.cache.clear(model_name.route_key)
+      Rails.cache.clear
       true
     end
 
