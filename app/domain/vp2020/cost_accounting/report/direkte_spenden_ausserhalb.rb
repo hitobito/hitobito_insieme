c0ef6565e2d2fe2a019f9468945e9002e4467e4d
Matthias Viehweger <viehweger@puzzle.ch>
2020-08-04 16:36:36 +0200

    Remove 'Abgrenzung Dachorganisation' from all VP2020-Forms

--- app/domain/vp2020/cost_accounting/report/direkte_spenden_ausserhalb.rb
+++ app/domain/vp2020/cost_accounting/report/direkte_spenden_ausserhalb.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2014, insieme Schweiz. This file is part of
+#  Copyright (c) 2012-2020, insieme Schweiz. This file is part of
 #  hitobito_insieme and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_insieme.
@@ -14,8 +14,7 @@ module Vp2020::CostAccounting
       self.aufwand = false
 
       delegate_editable_fields %w(aufwand_ertrag_fibu
-                                  aufteilung_kontengruppen
-                                  abgrenzung_dachorganisation)
+                                  aufteilung_kontengruppen)
 
       alias_method :abgrenzung_fibu, :aufwand_ertrag_fibu
 
--- app/domain/vp2020/cost_accounting/report/time_distributed.rb
+++ app/domain/vp2020/cost_accounting/report/time_distributed.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2014, insieme Schweiz. This file is part of
+#  Copyright (c) 2012-2020, insieme Schweiz. This file is part of
 #  hitobito_insieme and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_insieme.
@@ -23,8 +23,7 @@ module Vp2020::CostAccounting
 
       delegate_editable_fields %w(aufwand_ertrag_fibu
                                   aufteilung_kontengruppen
-                                  abgrenzung_fibu
-                                  abgrenzung_dachorganisation)
+                                  abgrenzung_fibu)
 
       delegate :time_record, to: :table
 
