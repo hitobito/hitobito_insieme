190da2d1f9c30994085285fb7974af640b1f7487
Matthias Viehweger <viehweger@puzzle.ch>
2020-08-04 18:27:04 +0200

    fixup Remove Abgrenzung Dachorga

--- app/domain/vp2020/cost_accounting/report/indirekte_spenden.rb
+++ app/domain/vp2020/cost_accounting/report/indirekte_spenden.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2014, insieme Schweiz. This file is part of
+#  Copyright (c) 2012-2020, insieme Schweiz. This file is part of
 #  hitobito_insieme and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_insieme.
@@ -15,7 +15,6 @@ module Vp2020::CostAccounting
 
       delegate_editable_fields %w(aufwand_ertrag_fibu
                                   aufteilung_kontengruppen
-                                  abgrenzung_dachorganisation
 
                                   beratung
                                   medien_und_publikationen
