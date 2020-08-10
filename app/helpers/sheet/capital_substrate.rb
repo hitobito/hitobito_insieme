568596f7df42c5d7dbbc4503b9839e80a14373c9
Matthias Viehweger <viehweger@puzzle.ch>
2020-08-10 16:46:55 +0200

    Wrap capital-substrate in their own sheet

---
+++ app/helpers/sheet/capital_substrate.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2020, Insieme Schweiz. This file is part of
+#  hitobito_insieme and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_insieme.
+
+module Sheet
+  class CapitalSubstrate < Base
+    self.parent_sheet = Sheet::Group
+  end
+end
