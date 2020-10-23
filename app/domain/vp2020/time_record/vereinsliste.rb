6fa82e8f536c1b0e4b3af0ce2e7f93670ebd7509
Matthias Viehweger <viehweger@puzzle.ch>
2020-10-23 23:36:22 +0200

    Conform to linter and styleguide

--- app/domain/vp2020/time_record/vereinsliste.rb
+++ app/domain/vp2020/time_record/vereinsliste.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2016, insieme Schweiz. This file is part of
+#  Copyright (c) 2020, insieme Schweiz. This file is part of
 #  hitobito_insieme and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_insieme.
@@ -17,13 +17,14 @@ module Vp2020
 
     def vereine
       @vereine ||=
-        Group.
-        without_deleted.
-        where(type: [Group::Dachverein,
-                     Group::Regionalverein,
-                     Group::ExterneOrganisation].
-                       collect(&:sti_name)).
-      order_by_type
+        Group
+        .without_deleted
+        .where(type: [
+          Group::Dachverein,
+          Group::Regionalverein,
+          Group::ExterneOrganisation
+        ].collect(&:sti_name))
+        .order_by_type
     end
 
     def time_record(verein)
@@ -31,11 +32,7 @@ module Vp2020
     end
 
     def time_records
-      @time_records ||=
-        ::TimeRecord.where(type: type, year: year).
-        each_with_object({}) do |record, hash|
-          hash[record.group_id] = record
-        end
+      @time_records ||= ::TimeRecord.where(type: type, year: year).index_by(&:group_id)
     end
 
   end
