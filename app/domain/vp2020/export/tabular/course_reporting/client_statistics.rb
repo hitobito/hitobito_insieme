c0c8e25785d9d41195561d46218e1fd5bc7f44df
Matthias Viehweger <viehweger@puzzle.ch>
2021-01-15 09:50:38 +0100

    Add new columns

--- app/domain/vp2020/export/tabular/course_reporting/client_statistics.rb
+++ app/domain/vp2020/export/tabular/course_reporting/client_statistics.rb
@@ -23,6 +23,7 @@ module Vp2020::Export
 
         attr_reader :stats
 
+        # Vp2020::CourseReporting::ClientStatistics
         def initialize(stats)
           @stats = stats
         end
@@ -47,6 +48,9 @@ module Vp2020::Export
           [
             vp_t('group_or_course_type'),
             vp_t('course_fachkonzept'),
+            vp_t('course_count'),
+            vp_t('course_hours'),
+            vp_t('other_attendees'),
             vp_t('course_total')
           ] + @stats.cantons.map do |canton|
             attr_t("event/participation_canton_count.#{canton}")
@@ -56,21 +60,59 @@ module Vp2020::Export
         private
 
         def empty_row
-          Array.new(stats.cantons.size + 3, nil)
+          Array.new(stats.cantons.size + 6, nil)
         end
 
         def group_label(group)
-          [group.name, nil, nil] + stats.cantons.map { |_| nil }
+          [group.name, nil, nil, nil, nil, nil] + stats.cantons.map { |_| nil }
         end
 
         def group_stats(group_id, lk, fachkonzept)
+          gcp = stats.group_participants(group_id, lk, fachkonzept)
           [
             attr_t("event/course.leistungskategorien.#{lk}", count: 3),
             vp_t("fachkonzept.#{fachkonzept}"),
-            maybe_zero(stats.group_canton_count(group_id, 'total', lk, fachkonzept))
+            gcp.course_count.presence,
+            kursdauer_und_treffpunkt_grundlagenanteil(gcp),
+            maybe_zero(gcp.other_attendees.to_i),
+            maybe_zero(gcp.total.to_i)
           ] + stats.cantons.map do |canton|
-            maybe_zero(stats.group_canton_count(group_id, canton, lk, fachkonzept))
+            maybe_zero(gcp.send(canton.to_sym).to_i)
           end
+        end
+
+        def kursdauer_und_treffpunkt_grundlagenanteil(gcp)
+          return gcp.course_hours unless gcp.fachkonzept == 'treffpunkt'
+
+          # additionally to the reported hours, we shall add the hours reported for basic work,
+          # in the same proportion of tp/all-courses
+          #
+          # keep in mind: logic != business logic !== government logic ;-)
+
+          grundlagen = TimeRecord.where(group_id: gcp.group_id, year: @year).sum(:kurse_grundlagen).to_f
+
+          kostenrechnung = vp_class('CostAccounting::Table').new(Group.find(gcp.group_id), @year)
+
+          vollkosten_tp = kostenrechnung.value_of('vollkosten', 'treffpunkte').to_f
+          vollkosten_alle =
+            if vollkosten_tp.positive?
+              vollkosten_tp +
+                kostenrechnung.value_of('vollkosten', 'blockkurse').to_f +
+                kostenrechnung.value_of('vollkosten', 'tageskurse').to_f +
+                kostenrechnung.value_of('vollkosten', 'jahreskurse').to_f
+            else
+              0
+            end
+
+          anteil =
+            if vollkosten_alle.positive?
+              (grundlagen * ( vollkosten_tp / vollkosten_alle))
+            else
+              0
+            end
+
+          result = gcp.course_hours.to_f + anteil
+          maybe_zero(result)
         end
 
         def maybe_zero(number)
