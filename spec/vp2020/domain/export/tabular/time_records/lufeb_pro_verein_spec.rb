0448ab034a01919812cdc2d25e8ac357b86d8198
Matthias Viehweger <viehweger@puzzle.ch>
2021-01-27 21:11:50 +0100

    Backfill spec for implemented export

--- spec/vp2020/domain/export/tabular/time_records/lufeb_pro_verein_spec.rb
+++ spec/vp2020/domain/export/tabular/time_records/lufeb_pro_verein_spec.rb
@@ -6,7 +6,42 @@
 #  https://github.com/hitobito/hitobito_insieme.
 
 describe Vp2020::Export::Tabular::TimeRecords::LufebProVerein do
-  it 'needs to be implemented' do
-    expect(true).to be_falsey
+  let(:year) { 2020 }
+  subject { described_class.new(data) }
+
+  let(:data) do
+    lpv = Vp2020::TimeRecord::LufebProVerein.new(year)
+    verein_ids = lpv.vereine.map(&:id)
+
+    lpv.instance_variable_set('@lufeb_data', {
+      verein_ids[0] => Vp2020::TimeRecord::LufebProVerein::Data.new(1, 2, 3, 4, 5),
+      verein_ids[1] => Vp2020::TimeRecord::LufebProVerein::Data.new(2, 4, 6, 8, 9),
+      verein_ids[2] => Vp2020::TimeRecord::LufebProVerein::Data.new(Array.new(5) {0}),
+      verein_ids[3] => Vp2020::TimeRecord::LufebProVerein::Data.new(Array.new(5) {0})
+    })
+
+    lpv
+  end
+
+  it 'has labels' do
+    expected = ['Gruppe/Feldname', '']
+
+    expect(subject.labels).to match_array expected
+    expect(subject.labels).to eq expected
+  end
+
+  it 'has all the calculated values' do
+    rows = subject.data_rows.take(10)
+
+    expect(rows[0]).to be_all nil
+    expect(rows[1]).to eq ['insieme Schweiz',                           nil]
+    expect(rows[2]).to eq ['Allgemeine Medien & Öffentlichkeitsarbeit',   1]
+    expect(rows[3]).to eq ['Themenspezifische Grundlagenarbeit',         11]
+    expect(rows[4]).to eq ['Förderung der Selbsthilfe',                   3]
+    expect(rows[5]).to be_all nil
+    expect(rows[6]).to eq ['Kanton Bern',                               nil]
+    expect(rows[7]).to eq ['Allgemeine Medien & Öffentlichkeitsarbeit',   2]
+    expect(rows[8]).to eq ['Themenspezifische Grundlagenarbeit',         21]
+    expect(rows[9]).to eq ['Förderung der Selbsthilfe',                   6]
   end
 end
--- spec/vp2020/domain/time_record/lufeb_pro_verein_spec.rb
+++ spec/vp2020/domain/time_record/lufeb_pro_verein_spec.rb
@@ -5,8 +5,71 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_insieme.
 
+require 'spec_helper'
+
 describe Vp2020::TimeRecord::LufebProVerein do
-  it 'needs to be implemented' do
-    expect(true).to be_falsey
+  let(:year) { 2020 }
+  subject { described_class.new(year) }
+
+  let(:group) { groups(:dachverein) }
+
+  it 'knows a list of groups to export' do
+    expected = [
+      'insieme Schweiz',
+      'Kanton Bern',
+      'Biel-Seeland',
+      'Freiburg'
+    ]
+
+    expect(subject.vereine.map(&:name)).to match_array expected
+    expect(subject.vereine.map(&:name)).to eq expected
+  end
+
+
+  it 'has a structured data-object for all groups' do
+    list = subject.vereine.map { |verein| subject.lufeb_data_for(verein.id) }
+
+    expect(list).to be_all Vp2020::TimeRecord::LufebProVerein::Data
+  end
+
+  it 'has correct data' do
+    tr_mit = create_report(
+      TimeRecord::EmployeeTime,
+      auskuenfte: 1,
+      gremien: 2,
+      beratung_fachhilfeorganisationen: 3,
+      lufeb_grundlagen: 4,
+      kurse_grundlagen: 5,
+    )
+
+    tr_vol = create_report(
+      TimeRecord::VolunteerWithVerificationTime,
+      auskuenfte: 5,
+      gremien: 4,
+      beratung_fachhilfeorganisationen: 3,
+      lufeb_grundlagen: 2,
+      kurse_grundlagen: 1,
+    )
+
+    expect(tr_mit).to be_valid
+    expect(tr_vol).to be_valid
+
+    expect(
+      tr_mit.total_lufeb_general
+    ).to eq 1
+
+    data = subject.lufeb_data_for(group.id)
+
+    expect(data.general).to eq 6
+    expect(data.specific).to eq 6
+    expect(data.promoting).to eq 6
+    expect(data.lufeb_grundlagen).to eq 6
+    expect(data.kurse_grundlagen).to eq 6
+  end
+
+  private
+
+  def create_report(model_name, values)
+    model_name.create!(values.merge(group_id: group.id, year: year))
   end
 end
