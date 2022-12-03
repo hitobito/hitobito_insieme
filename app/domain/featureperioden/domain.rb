ef3d727e7873fd62cda0abd4ac81344bc05a93e4
Matthias Viehweger <viehweger@puzzle.ch>
2022-12-03 17:04:27 +0100

    Give some debugging hints in development

--- app/domain/featureperioden/domain.rb
+++ app/domain/featureperioden/domain.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2020, Insieme Schweiz. This file is part of hitobito_insieme
+#  Copyright (c) 2020-2022, Insieme Schweiz. This file is part of hitobito_insieme
 #  and licensed under the Affero General Public License version 3 or later.
 #  See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_insieme.
@@ -16,11 +16,15 @@ module Featureperioden
     end
 
     def fp_class(class_name)
-      featureperiode.domain_class(class_name)
+      featureperiode.domain_class(class_name).tap do |fp_class_name|
+        Rails.logger.debug "FP: Domain class #{fp_class_name}"
+      end
     end
 
     def fp_i18n_scope(controller_name)
-      featureperiode.i18n_scope(controller_name)
+      featureperiode.i18n_scope(controller_name).tap do |fp_i18n_scope|
+        Rails.logger.debug "FP: I18n-scope #{fp_i18n_scope}"
+      end
     end
   end
 end
