f7870d861e3d0215978c7d272657dc37aaf6fc6c
Daniel Illi <illi@puzzle.ch>
2022-12-09 16:26:38 +0100

    Adapt fp2022/export/tabular/time_records/lufeb_pro_verein.rb

--- spec/domain/fp2022/export/tabular/time_records/lufeb_pro_verein_spec.rb
+++ spec/domain/fp2022/export/tabular/time_records/lufeb_pro_verein_spec.rb
@@ -49,36 +49,12 @@ describe Fp2022::Export::Tabular::TimeRecords::LufebProVerein do
     expect(rows[9]).to eq ['Förderung der Selbsthilfe',                   6]
   end
 
-  context 'adds a part of a grundlagenarbeit to specific, it' do
+  context 'adds grundlagenarbeit to specific, it' do
     let(:group) { groups(:seeland) }
 
     subject(:lufeb_verein_data) do
       # Struct.new(:general, :specific, :promoting, :lufeb_grundlagen, :kurse_grundlagen)
       fp_class('TimeRecord::LufebProVerein::Data').new(nil, 30, nil, 10, 20)
-    end
-
-    it 'fetches various parts from the kostenrechnung and sums them' do
-      kostenrechnung = fp_class('CostAccounting::Table').new(group, year)
-
-      expect(kostenrechnung).to receive(:value_of).with('vollkosten', 'jahreskurse').and_return(1495.24.to_d)
-      expect(kostenrechnung).to receive(:value_of).with('vollkosten', 'blockkurse').and_return(4990.48.to_d)
-      expect(kostenrechnung).to receive(:value_of).with('vollkosten', 'tageskurse').and_return(1497.62.to_d)
-      expect(kostenrechnung).to receive(:value_of).with('vollkosten', 'treffpunkte').and_return(3244.05.to_d)
-
-      total = [
-        kostenrechnung.value_of('vollkosten', 'jahreskurse'),
-        kostenrechnung.value_of('vollkosten', 'blockkurse'),
-        kostenrechnung.value_of('vollkosten', 'tageskurse'),
-        kostenrechnung.value_of('vollkosten', 'treffpunkte'),
-      ].map(&:to_f).sum
-
-      expect(total).to be_within(0.01).of(11227.39)
-    end
-
-    it 'derives the anteil from the kostenrechnung' do
-      anteil = ((11227.39.to_f - 3244.05.to_f) / 11227.39.to_f)
-
-      expect(anteil).to be_within(0.001).of(0.711)
     end
 
     it 'takes data from the lufeb-for-verein-data' do
@@ -87,34 +63,19 @@ describe Fp2022::Export::Tabular::TimeRecords::LufebProVerein do
       expect(lufeb_verein_data.kurse_grundlagen.to_f).to eq 20
     end
 
-    it 'adds the anteil to the specifics' do
-      result = 30 + 10 + (20 * 0.711)
-
-      expect(result).to be_within(0.001).of(54.221)
-    end
-
     it 'matches the actual implementation result' do
       # collaborators
-      kostenrechnung = double("CostAccounting-Table")
       stats = double('LUFEB pro Verein-stats')
 
       # return-values from collbarators
       allow(stats).to receive(:year).and_return(year) # from let above
       expect(stats).to receive(:lufeb_data_for).with(group.id).and_return(lufeb_verein_data)
 
-      expect(kostenrechnung).to receive(:value_of).with('vollkosten', 'jahreskurse').and_return(1495.24.to_d)
-      expect(kostenrechnung).to receive(:value_of).with('vollkosten', 'blockkurse').and_return(4990.48.to_d)
-      expect(kostenrechnung).to receive(:value_of).with('vollkosten', 'tageskurse').and_return(1497.62.to_d)
-      expect(kostenrechnung).to receive(:value_of).with('vollkosten', 'treffpunkte').and_return(3244.05.to_d)
-
-      # wire collaborators together
-      expect(fp_class('CostAccounting::Table')).to receive(:new).with(group, year).and_return(kostenrechnung)
-
       # execute
       result = described_class.new(stats)
                               .send(:specific_with_grundlagen, group)
 
-      expect(result).to be_within(0.001).of(54.221)
+      expect(result).to be_within(0.001).of(60)
     end
   end
 end
