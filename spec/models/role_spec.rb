832eae5b3f517b09e0dbb76b6ab5fab39031a8db
Nils Rauch <rauch@puzzle.ch>
2023-04-04 08:17:41 +0200

    Remove core role constants since they are not initialized

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,29 +1,14 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
 #  Copyright (c) 2012-2022, insieme Schweiz. This file is part of
 #  hitobito_insieme and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_insieme.
 
-describe Role do
-    # we don't want to check the test roles of the base project
-    base_project_testroles = [
-      Group::BottomGroup::Leader,
-      Group::BottomGroup::Member,
-      Group::BottomLayer::Leader,
-      Group::BottomLayer::LocalGuide,
-      Group::BottomLayer::Member,
-      Group::GlobalGroup::Leader,
-      Group::GlobalGroup::Member,
-      Group::TopGroup::Leader,
-      Group::TopGroup::LocalGuide,
-      Group::TopGroup::Secretary,
-      Group::TopGroup::LocalSecretary,
-      Group::TopGroup::Member,
-      Group::TopLayer::TopAdmin,
-    ]
+require 'spec_helper'
 
-    (Role.subclasses - base_project_testroles).each do |role|
+describe Role do
+    Role.subclasses.each do |role|
       if role.permissions.present?
         it "#{role.name} has permissions so must have two_factor_authentication_enforced=true" do
           expect(role).to be_two_factor_authentication_enforced
