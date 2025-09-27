028f8815e93103a20afac04207f66ac9e8b05781
Serafin Schaufelberger <138892893+sschaufelberger@users.noreply.github.com>
2025-09-27 12:23:22 +0200

    Update fsio2428/v10.rb

--- app/domain/policies/fsio2428/v10.rb
+++ app/domain/policies/fsio2428/v10.rb
@@ -1,9 +1,7 @@
 module Policies
     module Fsio2428
         class V10
-            def include_grundlagen_hours
-                true
-            end
+            def include_grundlagen_hours? = true
         end
     end
 end
