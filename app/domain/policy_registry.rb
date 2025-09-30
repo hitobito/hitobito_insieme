61706e5ccf810d4f12557d08fb2dc5ec3c29014e
Serafin Schaufelberger <138892893+sschaufelberger@users.noreply.github.com>
2025-09-30 08:52:12 +0200

    activate policy V11 for years 2025+

--- app/domain/policy_registry.rb
+++ app/domain/policy_registry.rb
@@ -4,8 +4,8 @@ class PolicyRegistry
     case year.to_i
     when 2024
       Policies::Fsio2428::V10.new
-    #when 2025..Float::INFINITY
-      #Policies::Fsio2428::V11.new
+    when 2025..Float::INFINITY
+      Policies::Fsio2428::V11.new
     else
       Policies::Fsio2428::V10.new
     end
