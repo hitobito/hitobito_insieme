72d278e9c2504e6cfb6cf8ecabfb4416ce6fe6b6
Serafin Schaufelberger <138892893+sschaufelberger@users.noreply.github.com>
2025-10-07 11:26:06 +0200

    Add license and explanatory comment

--- app/domain/fp2024.rb
+++ app/domain/fp2024.rb
@@ -1,1 +1,9 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2020-2021, Insieme Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_insieme.
+
+# An empty Fp2024 shell that defines the starting point for BSV-Vertragsperiode 2024-2028.
 module Fp2024; end
