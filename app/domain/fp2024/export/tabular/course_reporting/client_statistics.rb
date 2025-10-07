ffe612f36b554bb8ee3f6abe3bd1a153d8a369c9
Serafin Schaufelberger <138892893+sschaufelberger@users.noreply.github.com>
2025-10-07 11:20:33 +0200

    Add license

--- app/domain/fp2024/export/tabular/course_reporting/client_statistics.rb
+++ app/domain/fp2024/export/tabular/course_reporting/client_statistics.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+#  Copyright (c) 2020-2025, Insieme Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_insieme.
+
 module Fp2024
   module Export
     module Tabular
