5a70a47321a0b299d73244102aa32fd5d9645388
Serafin Schaufelberger <138892893+sschaufelberger@users.noreply.github.com>
2025-10-07 11:22:28 +0200

    Add license and explanatory comment

--- app/domain/policies/fsio2428/v10.rb
+++ app/domain/policies/fsio2428/v10.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2020-2021, Insieme Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_insieme.
+
+# Baseline policy for BSV-Vertragsperiode 2024-2028.
+# It includes Grundlagenarbeit for all Leitsungskategorien (Bk, SK, TK, TP)
 module Policies
   module Fsio2428
     class V10
